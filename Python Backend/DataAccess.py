5797cbe666677c1a6bb8beef7e39a9b395842dbb
FreekDS <freek.desagher@student.uantwerpen.be>
2019-02-27 16:28:19 +0100

    Added simple function to create research groups

--- Python Backend/DataAccess.py
+++ Python Backend/DataAccess.py
@@ -7,11 +7,11 @@ class DataAccess:
         self.dbconnect = dbconnect
 
     def get_document(self):
-        cursor=self.dbconnect.get_cursor()
+        cursor = self.dbconnect.get_cursor()
         cursor.execute('SELECT * FROM document')
-        documents=list()
+        documents = list()
         for row in cursor:
-            document=Document(row[0])
+            document = Document(row[0])
             documents.append(document)
         return documents
 
@@ -28,17 +28,18 @@ class DataAccess:
     def get_researchGroup(self):
         cursor = self.dbconnect.get_cursor()
         cursor.execute('select * from researchGroup')
-        rgroups=list()
+        rgroups = list()
         for row in cursor:
-            rgroup=ResearchGroup(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+            rgroup = ResearchGroup(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
             rgroups.append(rgroup)
         return rgroups
 
-    def add_researchGroup(self,group):
+    def add_researchGroup(self, group):
         cursor = self.dbconnect.get_cursor()
         try:
             cursor.execute('INSERT INTO researchGroup values(%s,%s,%s,%s,%s,%s,%s)',
-                           (group.name,group.abbreviation,group.discipline,group.active,group.adress,group.telNr,group.groupDescription))
+                           (group.name, group.abbreviation, group.discipline, group.active, group.address, group.telNr,
+                            group.desc))
             # get id and return updated object
             self.dbconnect.commit()
         except:
