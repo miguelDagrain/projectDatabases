b57da7a1b20eb18cd6eb8d3a2a713118fe846150
thibautvangoethem <Thibaut.VanGoethem@student.uantwerpen.be>
2019-03-02 13:37:54 +0100

    merge tijd

--- Python Backend/Document.py
+++ Python Backend/Document.py
@@ -1,11 +1,20 @@
+from enum import Enum
+
+
+class language(Enum):
+    none = 0
+    NEDERLANDS = 1
+    ENGELS = 2
+
 class Document:
-    def __init__(self,id, text):
+    def __init__(self,id,lang, text):
         """
         constructor for dbdocument with text given
         :param text: the text that is contained in the document
         :return: a new dbDocument object
         """
         self.ID=id
+        self.language=lang
         self.text = text
 
 
