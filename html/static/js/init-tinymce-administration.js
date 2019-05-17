6ed7435b82750ad046d8a7b399c906061fd79071
oli4 <olivier.van.houtte.sonic@gmail.com>
2019-05-17 14:00:23 +0200

    Added: - Button to add file link inside the html. - Still bugged... usage not recommended.

--- html/static/js/init-tinymce-administration.js
+++ html/static/js/init-tinymce-administration.js
@@ -8,8 +8,56 @@ tinymce.init({
 
     },
 
+    schema: "html4",
     plugins: "link code",
     menubar: 'file edit insert view format table tools help',
+    menu: {
+        insert: { title: "insert", items: "link linktofile" }
+    },
+
+    setup: function (editor) {
+
+        editor.ui.registry.addMenuItem('linktofile', {
+            text: 'link to file',
+            icon: 'link',
+            onAction: function () {
+
+
+                editor.windowManager.open({
+                    title: 'Link file',
+                    size: 'normal',
+                    body: [
+                        {
+                            title: 'General',
+                            type: 'panel',
+                            name: 'general'
+                        },
+                    ],
+                    buttons: [{
+                        type: 'submit',
+                        name: 'save',
+                        text: 'Save',
+                        primary: true,
+                        onclick: function() {
+                            // TODO: handle primary btn click
+                            (this).parent().parent().close();
+                        }
+                    },
+                        {
+                            type: 'cancel',
+                            name: 'cancel',
+                            text: 'Cancel',
+                            onclick: function() {
+                                (this).parent().parent().close();
+                            }
+                        }]
+
+                });
+
+            }
+        });
+
+    },
 
     theme: "silver",
     skin: "oxide",
