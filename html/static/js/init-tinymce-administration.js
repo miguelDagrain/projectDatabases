708a3e176975a045ad5edd48e5779c607cf7e2ad
miguelDagrain <Miguel.Dagrain@student.uantwerpen.be>
2019-05-24 10:42:58 +0200

    init file

--- html/static/js/init-tinymce-administration.js
+++ html/static/js/init-tinymce-administration.js
@@ -7,6 +7,89 @@ tinymce.init({
             tinymce.triggerSave();
         });
 
+
+        var openDialog = function () {
+
+            var files = document.getElementById("administration-form-upload").firstChild;
+
+            var myListItems = [];
+
+            while (files != null){
+                myListItems.push(files.innerHTML.toString());
+                files = files.nextSibling;
+            }
+
+            var itemss = [];
+            tinymce.each(myListItems, function (myListItemName) {
+                itemss.push({
+                    text: myListItemName,
+                    value: myListItemName
+                });
+            });
+
+
+            return editor.windowManager.open({
+                title: 'Example plugin',
+                body: {
+                    type: 'panel',
+                    items: [
+                        {
+                            type: 'selectbox',
+                            name: 'title',
+                            label: 'Title',
+                            items: itemss
+                        },
+                        {
+                            type: 'input',
+                            name: 'Text',
+                            label: 'Text to display'
+                        }
+                    ]
+                },
+                buttons: [
+                    {
+                        type: 'cancel',
+                        text: 'Close'
+                    },
+                    {
+                        type: 'submit',
+                        text: 'Save',
+                        primary: true
+                    }
+                ],
+                onSubmit: function (api) {
+                    var data = api.getData();
+                    // Insert content when the window form is submitted
+
+                    var a = document.createElement("a");
+
+                    if (dl === "../download/"){
+
+                        var t = document.getElementById("administration-form-title").value.toString();
+
+                        a.href= dl + t + "_" + data.title;
+                    }else{
+                        a.href= dl + data.title;
+                    }
+
+                    a.appendChild(document.createTextNode(data.Text));
+
+                    editor.insertContent(a.outerHTML.toString());
+                    api.close();
+                }
+            });
+
+        };
+
+        editor.ui.registry.addMenuItem('linktofile', {
+               text: 'link to file',
+               icon: 'link',
+               onAction: function () {
+                   // Open window
+                   openDialog();
+
+               }
+        });
     },
 
     schema: "html4",
@@ -15,93 +98,6 @@ tinymce.init({
     menu: {
         insert: { title: "insert", items: "link linktofile" }
     },
-
-        setup: function (editor) {
-
-            var openDialog = function () {
-
-                var files = document.getElementById("administration-form-upload").firstChild;
-
-                var myListItems = [];
-
-                while (files != null){
-                    myListItems.push(files.innerHTML.toString());
-                    files = files.nextSibling;
-                }
-
-                var itemss = [];
-                tinymce.each(myListItems, function (myListItemName) {
-                    itemss.push({
-                        text: myListItemName,
-                        value: myListItemName
-                    });
-                });
-
-
-                return editor.windowManager.open({
-                    title: 'Example plugin',
-                    body: {
-                        type: 'panel',
-                        items: [
-                            {
-                                type: 'selectbox',
-                                name: 'title',
-                                label: 'Title',
-                                items: itemss
-                            },
-                            {
-                                type: 'input',
-                                name: 'Text',
-                                label: 'Text to display'
-                            }
-                        ]
-                    },
-                    buttons: [
-                        {
-                            type: 'cancel',
-                            text: 'Close'
-                        },
-                        {
-                            type: 'submit',
-                            text: 'Save',
-                            primary: true
-                        }
-                    ],
-                    onSubmit: function (api) {
-                        var data = api.getData();
-                        // Insert content when the window form is submitted
-
-                        var a = document.createElement("a");
-
-                        if (dl === "../download/"){
-
-                            var t = document.getElementById("administration-form-title").value.toString();
-
-                            a.href= dl + t + "_" + data.title;
-                        }else{
-                            a.href= dl + data.title;
-                        }
-
-                        a.appendChild(document.createTextNode(data.Text));
-
-                        editor.insertContent(a.outerHTML.toString());
-                        api.close();
-                    }
-                });
-
-            };
-
-            editor.ui.registry.addMenuItem('linktofile', {
-                text: 'link to file',
-                icon: 'link',
-                onAction: function () {
-                    // Open window
-                    openDialog();
-
-                }
-            });
-
-        },
 
     theme: "silver",
     skin: "oxide",
